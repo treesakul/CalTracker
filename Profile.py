28fde394e8b79d63fec928378395ddbd12a47408
tinyzero <nine.tinyzero@gmail.com>
2017-05-29 15:34:46 +0700

    eiei

--- Profile.py
+++ Profile.py
@@ -1,5 +1,4 @@
 from pony.orm import *
-from datetime import *
 from Account import *
 db = Database()
 db.bind('oracle', 'TANAKORN/password@127.0.01')
@@ -8,7 +7,10 @@ class Profile(db.Entity):
     id = PrimaryKey(int)
     fname = Required(str)
     lname = Required(str)
-##    birthday = Required(date)
+    weight = Required(int)
+    height = Required(int)
+    birthday = Required(str)
+    goal = Required(int)
     pic = Optional(str)
     
 db.generate_mapping(create_tables= True)
@@ -18,8 +20,15 @@ def add_profile(iden, fname, lname):
     Profile(id = iden, fname = fname, lname = lname)
 
 @db_session
-def edit_profile(fname,lname,user,pasw,wei,hei):
-    pass
+def edit_profile(iden,fname,lname,user,pasw,wei,hei,bd,pic):
+    Profile[iden].fname = fname
+    Profile[iden].lname = lname
+    Profile[iden].weight = wei
+    Profile[iden].height = hei
+    Profile[iden].birthday = bd
+    Profile[iden].pic = pic
+    Account[iden].username = user
+    Account[iden].password = pasw
 
 @db_session
 def get():
